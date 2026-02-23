ed93cc01635b0967fc82d62ece2d52e9fe60139e
13YOG <88232045+a1uka@users.noreply.github.com>
2026-02-23 21:09:09 +0300

    init

---
+++ Input remover/main.c
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Your keyboard and mouse will blocked in 5 seconds...\n");
+
+    if (BlockInput(TRUE)) {
+        printf("Now only rebooting your PC will help...\n");
+        while (1) {
+            int i = 0;
+        }
+    }
+    else {
+        printf("Error: You should run this program with administartor permissions!\n");
+    }
+
+    return 0;
+}
